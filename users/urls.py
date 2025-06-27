c982129775d1ccfb77ba40f532af21486c012a5c
Medvedevy <walery.medvedev@gmail.com>
2025-06-27 19:41:03 +0300

    настроена маршрутизация API-эндпоинтов.

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet
+
+app_name = "users"
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+
+urlpatterns = router.urls
