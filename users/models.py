538cef64e3e40c42e8d8e9db9720a6cd5d840e09
Medvedevy <walery.medvedev@gmail.com>
2025-06-27 19:19:42 +0300

    добавлен модель пользователя, использующая email для авторизации

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Стандартная модель пользователя, использующая email для авторизации
+    """
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=35, blank=True, null=True, verbose_name="Телефон")
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name="Город")
+    avatar = models.EmailField(upload_to="users/avatars/", blank=True, null=True, verbose_name="Аватар")
+
+    USERNAME_FIELD = "email" # устанавливаем email как поля для авторизациии
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return sel.email
