94c3db9a604daeb4336e0abc58a861e83a3012aa
Medvedevy <walery.medvedev@gmail.com>
2025-06-27 19:38:32 +0300

    Созданы модели ViewSet для CRUD операций с курсами, Generic-класс для получения списка уроков и создания нового урока, Generic-класс для получения, обновления и удаления конкретного урока.

--- education/views.py
+++ education/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import viewsets, generics
+from education.models import Course, Lesson
+from education.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для CRUD операций с курсами.
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    """
+    Generic-класс для получения списка уроков и создания нового урока.
+    """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Generic-класс для получения, обновления и удаления конкретного урока.
+    """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
