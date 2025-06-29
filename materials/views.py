d7ecc9f1bf8b980e6dda26f83076bb4b273ffac0
Medvedevy <walery.medvedev@gmail.com>
2025-06-29 21:49:00 +0300

    Добавлен класс IsOwnerOrReadOnly, для проверки владельца

--- materials/views.py
+++ materials/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -20,14 +21,18 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = LessonSerializer
 
 
-class LessonListCreateView(generics.ListCreateAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        # Эта строка верна для Lesson, так как владелец через Course
+        return obj.course.course_user is not None and obj.course.course_user == request.user
 
 
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
     Generic-класс для получения, обновления и удаления конкретного урока.
     """
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
