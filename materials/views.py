78ae099208d95a0e283b89c0a1d8ed920ecb0029
Medvedevy <walery.medvedev@gmail.com>
2025-06-30 12:02:33 +0300

    Добавлен  def perform_create(self, serializer):         serializer.save(course_user=self.request.user)         чтобы автоматически назначать текущего пользователя владельцем курса

--- materials/views.py
+++ materials/views.py
@@ -3,14 +3,19 @@ from rest_framework import viewsets, generics
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticated
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     """
     ViewSet для CRUD операций с курсами.
     """
+    permission_classes = [IsAuthenticated]
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(course_user=self.request.user)
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
