d95009477ff46d6801cf71eacbc2475233b95f48
Medvedevy <walery.medvedev@gmail.com>
2025-06-30 16:04:55 +0300

    зарегистрировали PaymentViewSet

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ app_name = "users"
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"payments", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("profile/", ProfileUpdateView.as_view(), name="profile-update"),
