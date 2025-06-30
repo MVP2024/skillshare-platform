94a821c846699d9566991a68155d98df701841bb
Medvedevy <walery.medvedev@gmail.com>
2025-06-30 16:05:59 +0300

    Добавил докстринг и комменты

--- materials/management/commands/load_initial_data.py
+++ materials/management/commands/load_initial_data.py
@@ -7,21 +7,37 @@ from users.models import User
 
 
 class Command(BaseCommand):
+    """
+        Команда Django для загрузки начальных тестовых данных в базу данных.
+        Перед загрузкой данных, она удаляет существующие записи для предотвращения дублирования.
+        Данные загружаются из фикстуры 'initial_data.json', расположенной в 'materials/fixtures/'.
+    """
+
     help = "Загружает тестовые данные из фикстуры (курсы, уроки, пользователи, платежи)"
 
     def handle(self, *args, **options):
         self.stdout.write("Удаление данных...")
-        Lesson.objects.all().delete()  # Удаляем уроки первыми
-        Course.objects.all().delete()  # Затем курсы
-        User.objects.all().delete()  # Потом пользователей (каскадно удаляются платежи)
+        # Удаляем уроки первыми, так как они зависят от курсов
+        Lesson.objects.all().delete()
+
+        # Затем удаляем курсы
+        Course.objects.all().delete()
+
+        # Потом пользователей (каскадно удаляются связанные платежи)
+        User.objects.all().delete()
         self.stdout.write(self.style.SUCCESS("Данные удалены."))
 
         fixture_name = "initial_data.json"
+        # Формируем полный путь к файлу фикстуры
         fixture_path = Path(settings.BASE_DIR) / 'materials' / 'fixtures' / fixture_name
 
+        # Проверяем существование файла фикстуры перед загрузкой
         if fixture_path.exists():
             self.stdout.write("Загрузка фикстуры...")
+
+            # Вызываем встроенную команду Django 'loaddata' для загрузки данных
             call_command("loaddata", fixture_name)
             self.stdout.write(self.style.SUCCESS("Данные загружены."))
         else:
+            # Выводим предупреждение, если файл фикстуры не найден
             self.stdout.write(self.style.WARNING(f"Фикстура '{fixture_name}' не найдена."))
