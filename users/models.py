3f7a1fd69c2e20d42dbe533cd4e48a093f9c7235
Medvedevy <walery.medvedev@gmail.com>
2025-06-30 13:23:17 +0300

    добавлена модель платежа Payment.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -24,3 +26,77 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Paymant(models.Model):
+    """
+    Модель платежа.
+    Хрнит информацию о транзакциях пользователей за курсы или уроки.
+    """
+    # Варианты способов оплаты
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счет"),
+        ("make_qr_code", "Перевод по QR-коду"),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="payments",  # Добавляем related_name для удобства доступа из User
+        verbose_name="Пользователь",
+        help_text="Пользователь, совершивший платеж",
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата оплаты",
+        help_text="Дата и время совершения платежа",
+    )
+    paid_course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Оплаченный курс",
+        help_text="Курс, за который произведена оплата",
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Оплаченный урок",
+        help_text="Урок, за который произведена оплата",
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Сумма оплаты",
+        help_text="Сумма платежа",
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name="Способ оплаты",
+        help_text="Способ оплаты (наличные или перевод)",
+    )
+
+    class Meta:
+        verbose_name = "Платёж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        if self.paid_course:
+            return f"Платёж от {self.user.email} за курс '{self.paid_course.title}'"
+        elif self.paid_lesson:
+            return f"Платеж от {self.user.email} за урок '{self.paid_lesson.title}'"
+        return f"Платеж от {self.user.email} на сумму {self.amount}"
+
+    def clean(self):
+        """
+        Проверяет, что платеж связан либо с курсом, либо с уроком, но не с обоими.
+        """
+        if self.paid_course and self.paid_lesson:
+            raise ValidationError("Платеж не может быть одновременно за курс и за урок.")
+        if not self.paid_course and not self.paid_lesson:
+            raise ValidationError("Платеж должен быть связан либо с курсом, либо с уроком.")
