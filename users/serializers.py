f2bdcc5e294629a44aa878dac21cfc4dbc2668c6
Medvedevy <walery.medvedev@gmail.com>
2025-06-30 16:21:24 +0300

    1. Расширен UserSerializer, он теперь может выводить историю платежей пользователя. Пользователь не может редактировать платежи напрямую из-за поля read_only=True. 2. PaymentSerializerсоздан для корректного отображения платежей: 1. Использует PrimaryKeyRelatedField для оптимизации производительности. Вроде проще так, чем от вложенных объектов. Поддерживает поля, где курс/урок могут быть null(нулевыми).

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from users.models import User
+from users.models import Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+        Сериализатор для модели User, включающий историю платежей.
+        Поля:
+        - `payments`: Список всех платежей пользователя (вложенный сериализатор).
+    """
+
+    payments = PaymentSerializer(many=True, read_only=True)  # Вложенный сериализатор для платежей
+
     class Meta:
         model = User
         fields = (
@@ -13,4 +22,19 @@ class UserSerializer(serializers.ModelSerializer):
             "avatar",
             "first_name",
             "last_name",
+            "payments",
         )
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Payment, включающий информацию о пользователе, курсе и уроке.
+    Используется для вывода истории платежей в профиле пользователя.
+    """
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    paid_course = serializers.PrimaryKeyRelatedField(read_only=True, allow_null=True)
+    paid_lesson = serializers.PrimaryKeyRelatedField(read_only=True, allow_null=True)
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
