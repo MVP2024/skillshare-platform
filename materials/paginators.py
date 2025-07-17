a66a84065d531c087eb6c3398c983eb7d578aeec
Medvedevy <walery.medvedev@gmail.com>
2025-07-17 16:33:06 +0300

    Реализована пагинация для курсов и уроков

---
+++ materials/paginators.py
@@ -0,0 +1,14 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class MaterialsPagination(PageNumberPagination):
+    """
+    Пагинация для курсов и уроков.
+
+    - page_size: Количество элементов на странице (10)
+    - page_size_query_param: Параметр для изменения размера страницы
+    - max_page_size: Максимальное количество элементов на странице (100)
+    """
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
