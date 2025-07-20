7f6e50d1e1dc0ff4d1ee00b039a6f9342a44f5c8
Medvedevy <walery.medvedev@gmail.com>
2025-07-20 11:12:13 +0300

    Создан модуль views в корне проекта для документации JWT
    Создан файл `skillshare_platform/views.py` для размещения кастомных представлений JWT.
    Это позволяет применить декораторы `extend_schema` к эндпоинтам получения и обновления токенов,
    улучшая их описание в документации API (Swagger UI).

---
+++ skillshare_platform/views.py
@@ -0,0 +1,18 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.utils import extend_schema
+
+
+@extend_schema(
+    summary="Получение Access и Refresh токенов",
+    description="Принимает учетные данные пользователя (email, password) и возвращает Access и Refresh токены для аутентификации.",
+)
+class CustomTokenObtainPairView(TokenObtainPairView):
+    pass
+
+
+@extend_schema(
+    summary="Обновление Access Token",
+    description="Принимает Refresh Token и возвращает новый Access Token, если Refresh Token действителен.",
+)
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
