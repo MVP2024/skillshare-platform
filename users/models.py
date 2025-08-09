618d537458e1e41dfa1d1594586f2acc76edbc17
Medvedevy <walery.medvedev@gmail.com>
2025-08-09 14:02:27 +0300

    ничего не делал

--- users/models.py
+++ users/models.py
@@ -16,7 +16,7 @@ class CustomUserManager(BaseUserManager):
         Создает и сохраняет обычного пользователя с заданным email и паролем.
         """
         if not email:
-            raise ValueError("The Email field must be set")
+            raise ValueError("Поле Email должно быть заполнено")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -33,9 +33,9 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
         return self.create_user(email, password, **extra_fields)
 
 
@@ -87,7 +87,7 @@ class User(AbstractUser):
 class Payment(models.Model):
     """
     Модель платежа.
-    Хрнит информацию о транзакциях пользователей за курсы или уроки.
+    Хранит информацию о транзакциях пользователей за курсы или уроки.
     """
 
     # Варианты способов оплаты
